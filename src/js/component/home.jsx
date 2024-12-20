bcb5d5f1a6387e185fa37cd98a9d6e0dde541918
Jennifer Fernández <j.fdezmurga@gmail.com>
2024-12-20 21:42:52 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, {useState} from "react";
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+  const [tarea,setTarea] = useState ("");
+  const [elementoLista,setElementoLista] = useState([]);
+  //Añadir tarea a la lista
+  const añadirTarea = (e) => {
+    // console.log(event.target.type);
+    if (e.key ==="Enter" && tarea.trim() !==""){
+      setElementoLista([...elementoLista,tarea]); //[...,] Los 3 puntos añaden el elemento a la lista al pulsar enter
+      setTarea("");
+    }
+  }
+    //
+  const hacerClick = (eliminarIndice) => {
+    const newLi = elementoLista.filter ((__, linea) => linea !== eliminarIndice);//Método filter con el segúndo parámetro
+       setElementoLista(newLi);
+  };
+  return (
+    <div className="container d-flex flex-column mx-0 align-items-center mx-auto">
+        <h1 className="Titulo py-3">Lista de tareas</h1>
+        <div className="input-group w-50 m-0">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Añadir tarea aquí..."
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            onChange={(e)=> setTarea(e.target.value)}
+            onKeyDown={añadirTarea}
+            value={tarea}//Para que vuelva a quedarse en blanco al dar enter
+          />
+         </div>
+        {elementoLista.map((element, linea) => {
+          return (
+            <ul className="list-group w-50 d-flex justify-content-between">
+              <li className="list-group-item tarea-item d-flex justify-content-between " key={linea}>
+                {element}
+                <span className="delete-btn" onClick={() => hacerClick(linea)}><i className="fa-solid fa-xmark"></i></span>
+              </li>
+            </ul>
+          );
+        })}
+        <div className="list-group d-flex justify-content-center w-50">
+          <span className="list-group-item" id="tareas">{elementoLista.length} Pendientes.</span>
+        </div>
+    </div>
+  )
+  }
+  export default Home
